63463865a2fb5f67de424d73ecf6b1ffacd3cd11
PANTELIMONOV <pantelimonovmm@gmail.com>
2017-07-26 20:12:23 +0300

    Update test_group_modify.py

--- tests/test_group_modify.py
+++ tests/test_group_modify.py
@@ -1,6 +1,6 @@
-def test_modify_group(app, init_login, init_group):
-    data_for_changing = Group()
-    app.open_group_page()
-    app.group.modify(index, data_for_changing)
-    assert "........" in app.message()
-    app.return_to_group_page()
+# def test_modify_group(app, init_login, init_group):
+#     data_for_changing = Group()
+#     app.open_group_page()
+#     app.group.modify(index, data_for_changing)
+#     assert "........" in app.message()
+#     app.return_to_group_page()
